4495af3998363da7e5ec70929a6f0e6e96b5ce04
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-02-14 15:20:49 +0100

    Update overfitting_demo.py

--- Machine-Learning:Regression/week4/overfitting_demo.py
+++ Machine-Learning:Regression/week4/overfitting_demo.py
@@ -5,7 +5,7 @@ import math
 import random
 import matplotlib
 matplotlib.use('TkAgg')
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
 # Create random values for x in internal [0,1)
 
@@ -14,5 +14,22 @@ n = 30
 x = tc.SArray([random.random() for i in range(n)]).sort()
 
 # Compute y
-
 y = x.apply(lambda x: math.sin(4*x))
+
+# Add random Gaussian noise to y
+random.seed(1)
+e = tc.SArray([random.gauss(0,1.0/3.0) for i in range(n)])
+y = y + e
+
+# Pit data into an SFrame to manipulate later
+data = tc.SFrame({'X1': x, 'Y': y})
+# print(data)
+
+# Create a function to plot the data, since we will do in many times
+def plot_data(data):
+    plt.plot(data['X1'],data['Y'],'k.')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+# plot_data(data)
