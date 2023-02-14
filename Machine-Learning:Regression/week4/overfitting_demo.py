80d00da2d542aa166763ba1b8f3427b548b66d10
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-02-14 15:36:47 +0100

    Update overfitting_demo.py

--- Machine-Learning:Regression/week4/overfitting_demo.py
+++ Machine-Learning:Regression/week4/overfitting_demo.py
@@ -1,5 +1,5 @@
 # Create a dataset based on true sinusoidal relationship
-
+import numpy
 import turicreate as tc
 import math
 import random
@@ -31,5 +31,54 @@ def plot_data(data):
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show()
-
 # plot_data(data)
+
+# Define some useful polynomial regression functions
+def polynomial_features(data, deg):
+    data_copy = data.copy()
+    for i in range(1, deg):
+        data_copy['X'+str(i+1)] = data_copy['X'+str(i)]*data_copy['X1']
+    return data_copy
+
+# Define a function to fit a polynomial linear regression model of degree "deg" to the data in "data"
+def polynomial_regression(data, deg):
+    model = tc.linear_regression.create(polynomial_features(data, deg),
+                                        target='Y', l2_penalty=0., l1_penalty=0.,
+                                        validation_set=None, verbose=False)
+    return model
+
+# Define function to plat data and predictions made, since we are going to use it many times
+def plot_poly_predictions(data, model):
+    plot_data(data)
+
+    # Get the degree of teh polynomial
+    deg = len(model.coefficients['value'])-1
+
+    # Create 200 points in the x axis and compute the predicted value for each point
+    x_pred = tc.SFrame({'X1': [i/200.0 for i in range(200)]})
+    y_pred = model.predict(polynomial_features(x_pred, deg))
+
+    # plot predcictions
+    plt.plot(x_pred['X1'], y_pred, 'g-', label='degree' + str(deg) + ' fit')
+    plt.legend(loc='upper_left')
+    plt.axis([0, 1, -1.5, 2])
+    plt.show()
+
+# Create a function that prints the polynomial coefficients in a pretty way
+def print_coefficients(model):
+    deg = len(model.coefficients['value'])-1
+    # Get the degree of the polynomial
+
+    # Get learned parameters as a list
+    w = list(model.coefficients['value'])
+
+    # Numpy has a nifty function to print out polynomials in a pretty way
+    # (We'll use it, but it needs the parameters in the reverse order)
+    print('Learned polynomial for degree ' + str(deg) + ':')
+    w.reverse()
+    print(numpy.poly1d(w))
+
+# Fit a degree-2 polynomial
+model = polynomial_regression(data, deg=2)
+print_coefficients(model)
+plot_poly_predictions(data, model)
