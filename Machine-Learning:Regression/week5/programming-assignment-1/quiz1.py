0b574a31a00ace5f57a6196beeae07d5f88ae7a9
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-04-11 13:14:04 +0200

    quiz1 solved

--- Machine-Learning:Regression/week5/programming-assignment-1/quiz1.py
+++ Machine-Learning:Regression/week5/programming-assignment-1/quiz1.py
@@ -100,3 +100,66 @@ non_zero_index = [np.where(weights_all == weight)[0][0] for weight in weights_al
 selected_features = [all_features[index] for index in non_zero_index]
 # print(selected_features)
 
+testing['sqft_living_sqrt'] = testing['sqft_living'].apply(sqrt)
+testing['sqft_lot_sqrt'] = testing['sqft_lot'].apply(sqrt)
+testing['bedrooms_square'] = testing['bedrooms']*testing['bedrooms']
+testing['floors_square'] = testing['floors']*testing['floors']
+
+training['sqft_living_sqrt'] = training['sqft_living'].apply(sqrt)
+training['sqft_lot_sqrt'] = training['sqft_lot'].apply(sqrt)
+training['bedrooms_square'] = training['bedrooms']*training['bedrooms']
+training['floors_square'] = training['floors']*training['floors']
+
+validation['sqft_living_sqrt'] = validation['sqft_living'].apply(sqrt)
+validation['sqft_lot_sqrt'] = validation['sqft_lot'].apply(sqrt)
+validation['bedrooms_square'] = validation['bedrooms']*validation['bedrooms']
+validation['floors_square'] = validation['floors']*validation['floors']
+
+from sklearn.metrics import mean_squared_error
+
+RSS_VALIDATION = []
+for l1_penalty in np.logspace(1, 7, num=13):
+    model = linear_model.Lasso(alpha=l1_penalty, normalize=True)
+    model.fit(training[all_features], training['price'])
+    prediction = model.predict(validation[all_features])
+    RSS_VALIDATION.append(mean_squared_error(validation['price'], prediction))
+
+# print("the L1 penalty which produced the lower RSS on VALIDATION: ",
+#       np.logspace(1, 7, num=13)[RSS_VALIDATION.index(min(RSS_VALIDATION))])
+
+
+l1_penalty_q3 = 10.0
+model_q3 = linear_model.Lasso(alpha = l1_penalty_q3, normalize = True)
+model_q3.fit(training[all_features], training['price'])
+print("number of nonzero weights: ", (model_q3.coef_ != 0).sum() + (model_q3.intercept_ != 0).sum())
+
+
+max_nonzeros = 7
+
+def find_nonzero_weights(model):
+    return (model.coef_ != 0).sum() + (model.intercept_ != 0).sum()
+
+for l1_penalty in np.logspace(1, 4, num = 20):
+    model_q4 = linear_model.Lasso(alpha = l1_penalty, normalize = True)
+    model_q4.fit(training[all_features], training['price'])
+    print(l1_penalty, ":", find_nonzero_weights(model_q4))
+
+
+l1_penalty_min = 127.42749857
+l1_penalty_max = 263.665089873
+
+
+# for l1_penalty in np.linspace(l1_penalty_min, l1_penalty_max, 20):
+#     model_q5 = linear_model.Lasso(alpha = l1_penalty, normalize = True)
+#     model_q5.fit(training[all_features], training['price'])
+#     prediction_q5 = model_q5.predict(validation[all_features])
+#     RSS_q5 = mean_squared_error(prediction_q5, validation['price'])
+#     print(l1_penalty, RSS_q5, find_nonzero_weights(model_q5))
+
+
+model_q6 = linear_model.Lasso(alpha = 156.109096739, normalize = True)
+model_q6.fit(training[all_features], training['price'])
+
+weights_q6 = model_q6.coef_
+nonzero_index_q6 = [np.where(weights_q6 == weight)[0][0] for weight in weights_q6 if weight != 0.0]
+print([all_features[index] for index in nonzero_index_q6])
